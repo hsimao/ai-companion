94f18adfa790b6bc1a9b6f52ab9ab3baed8d69ae
hsimao <e087754958@gmail.com>
2023-08-19 15:33:52 +0800

    feat: render compions list page

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,14 +1,42 @@
 import { Categories } from '@/components/categories'
+import { Companions } from '@/components/companions'
 import { SearchInput } from '@/components/search-input'
 import prismadb from '@/lib/prismadb'
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    categoryId: string
+    name: string
+  }
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const companions = await prismadb.companion.findMany({
+    where: {
+      categoryId: searchParams.categoryId,
+      name: {
+        search: searchParams.name,
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+    // Count the number of messages
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  })
   const categories = await prismadb.category.findMany()
   console.log('categories', categories)
   return (
     <div className="h-full space-y-2 p-4">
       <SearchInput />
       <Categories data={categories} />
+      <Companions data={companions} />
     </div>
   )
 }
