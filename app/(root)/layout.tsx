2ca6809668ea0c4b1e0c647e964780f70cf558a9
hsimao <e087754958@gmail.com>
2023-08-30 21:50:16 +0800

    fix: navbar file name issues

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import { Navbar } from '@/components/navbar'
+import { Navbar } from '@/components/AppNavbar'
 import { Sidebar } from '@/components/sidebar'
 import { checkSubscription } from '@/lib/subscription'
 
